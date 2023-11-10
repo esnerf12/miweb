5471efbf9a7a67e8357e4251e52e03a6f9e754a9
Anderson Tovar <andersontovar09@gmail.com>
2023-11-10 09:23:51 -0400

    Funcionalidad de filtrar

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,25 +1,29 @@
-import { AddToCartIcon } from './Icons.jsx'
 import './Products.css'
+import { AddToCartIcon } from './Icons.jsx'
 
 export function Products ({ products }) {
-    return (
-        <main className='products'>
-            <ul>
-                {products.map(product => {
-                    <><li key={product.id}>
-                        <img
-                            src={product.thumbnail}
-                            alt={product.title} />
-                    </li><div>
-                            <strong>{product.title}</strong>
-                        </div><div>
-                            <button>
-                                <AddToCartIcon></AddToCartIcon>
-                            </button>
-                        </div></>
-                })
-                }
-            </ul>
-        </main>
-    )
+  return (
+    <main className='products'>
+      <ul>
+        {products.slice(0, 10).map(product => {
+          return (
+            <li key={product.id}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <div>
+                <strong>{product.title}</strong> - ${product.price}
+              </div>
+              <div>
+                <button>
+                    <AddToCartIcon></AddToCartIcon>
+                </button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
 }
